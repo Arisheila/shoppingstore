aa7d4305ee84110d77b40897b470c68f791e32d8
Arisheila <s.arinaitwe@student.ciu.ac.ug>
2023-10-13 11:10:40 +0300

    jest test

--- src/components/header/Header.test.js
+++ src/components/header/Header.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import "@testing-library/jest-dom"
+import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './Header';
 
@@ -11,38 +12,20 @@ jest.mock('react-router-dom', () => ({
 
 describe('Header', () => {
   test('renders header with navigation and icons', () => {
-    const { getByAltText, getByText } = render(
-      <Router>
-        <Header />
-      </Router>
-    );
-
-    // Check if the logo is rendered
-    const logo = getByAltText('shop-logo');
+ 
+    const { getByAltText, getByText } = 
+      render(
+        <Router>
+          <Header />
+        </Router>
+      );
+  
+      // Check if the logo is rendered
+  const logo = screen.getByAltText('shop-logo');
 
     console.log('logo', logo)
     expect(logo).toBeInTheDocument();
-
-    // Check if navigation links are rendered
-    // const homeLink = getByText('Home');
-    // const shopLink = getByText('Shop');
-    // const aboutLink = getByText('About');
-    // const contactLink = getByText('Contact');
-    // expect(homeLink).toBeInTheDocument();
-    // expect(shopLink).toBeInTheDocument();
-    // expect(aboutLink).toBeInTheDocument();
-    // expect(contactLink).toBeInTheDocument();
-
-    // Check if icons are rendered
-    // const cartIcon = getByText('Shopping Cart');
-    // const searchIcon = getByText('Search');
-    // const heartIcon = getByText('Heart');
-    // const userIcon = getByText('User');
-    // expect(cartIcon).toBeInTheDocument();
-    // expect(searchIcon).toBeInTheDocument();
-    // expect(heartIcon).toBeInTheDocument();
-    // expect(userIcon).toBeInTheDocument();
-  });
+    });
 });
 
 
