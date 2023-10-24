3ef506080834c4308112331b3c69ba05eb0820ac
Arisheila <s.arinaitwe@student.ciu.ac.ug>
2023-10-24 15:47:40 +0300

    login updates

--- src/components/header/LoginHeader.js
+++ src/components/header/LoginHeader.js
@@ -1,30 +1,64 @@
 /** @format */
 
 import React from "react"
+import "./Header.css"
+import shoplogo from "../banner/shoplogo.png"
 import { Link } from "react-router-dom"
 import { AiOutlineShoppingCart, AiOutlineSearch } from "react-icons/ai"
 import { HiOutlineHeart } from "react-icons/hi"
 import { TbUserExclamation } from "react-icons/tb"
+import { FaBars } from "react-icons/fa"
+import { RxCross2 } from "react-icons/rx"
 
 const LoginHeader = () => {
 	return (
 		<header>
 			<nav>
-				<ul className="nav-list">
-					<li className="nav-list-items">
-						<Link to="/" className="text-black">Home</Link>
+				<ul class="nav-bar">
+					<li class="logo">
+						<Link to="/">
+							<img
+								src={shoplogo}
+								alt="shop-logo"
+								className="banner-content-icon"
+							/>
+						</Link>
+						{/* <a href='#'><img src='./images/logo.png'/></a> */}
 					</li>
-					<li className="nav-list-items">
-						<Link to="/shop" className="text-black">Shop</Link>
-					</li>
-					<li className="nav-list-items">
-						<Link to="/about" className="text-black">About</Link>
-					</li>
-					<li className="nav-list-items">
-						<Link to="/contact" className="text-black">Contact</Link>
-					</li>
+					<input type="checkbox" id="check" />
+					<span class="menu">
+						<li className="nav-list">
+							<Link className="nav-list-link" to="/">
+								Home
+							</Link>
+						</li>
+						<li className="nav-list">
+							<Link className="nav-list-link" to="/shop">
+								Shop
+							</Link>
+						</li>
+						<li className="nav-list">
+							<Link className="nav-list-link" to="/about">
+								About
+							</Link>
+						</li>
+						<li className="nav-list">
+							<Link className="nav-list-link" to="/contact">
+								Contact
+							</Link>
+						</li>
+						<label for="check" class="close-menu">
+							<RxCross2 />
+							{/* <i class="fas fa-times"></i> */}
+						</label>
+					</span>
+					<label for="check" class="open-menu">
+						{/* <i class="fas fa-bars"></i> */}
+						<FaBars />
+					</label>
 				</ul>
 			</nav>
+
 			<div className="nav-icons">
 				<ul className="nav-icon-list">
 					<li className="nav-icon-list">
@@ -45,4 +79,4 @@ const LoginHeader = () => {
 	)
 }
 
-export default LoginHeader
+export default Header
